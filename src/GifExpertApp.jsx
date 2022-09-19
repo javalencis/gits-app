84962c79e00a0f99ac07acac8977b5f3a44c658e
Jonathan Valencia <javalencis@gmail.com>
2022-09-18 21:17:03 -0500

    Añadir categoria terminado

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,15 +1,42 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory"
 
 export const GifExpertApp = () => {
-  return (
-    <>
-        {/* Titulo */}
-        <h1>Git Expert App</h1>
-        
-        {/* Input */}
+    const [categories, setCategories] = useState(['Lol','Dragon ball'])
 
-        {/* Listado de Gif */}
+    const onAddCategory = (newCategory) => {
+        setCategories(categories => {
+            if(!categories.find(cat => cat.toLowerCase() === newCategory.toLowerCase())){
+                return [newCategory,...categories]
+            }else{
+                return [...categories]
+            }
+        })
+    }
 
+
+
+    return (
+        <>
+            {/* Titulo */}
+            <h1>Git Expert App</h1>
+   
+            {/* Input */}
+            <AddCategory onNewCategory={onAddCategory} />
+
+
+
+            {/* Listado de Gif */}
+            
+            <ol>
+                {categories.map((category,index) => {
+                    return <li key={index}>{category}</li>
+                })}
+            </ol>
+            
+            
+            
             {/* Git Item */}
-    </>
-  )
+        </>
+    )
 }
