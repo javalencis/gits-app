b15e9a5122a714d87ad0b9d6dad9ed6796f0223b
Jonathan Valencia <javalencis@gmail.com>
2022-09-18 19:17:50 -0500

    Primeros archivos del proyecto y estilos

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,15 @@
+
+export const GifExpertApp = () => {
+  return (
+    <>
+        {/* Titulo */}
+        <h1>Git Expert App</h1>
+        
+        {/* Input */}
+
+        {/* Listado de Gif */}
+
+            {/* Git Item */}
+    </>
+  )
+}
